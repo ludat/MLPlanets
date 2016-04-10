69f9f3a34e24e622e2441114b394f62b30874181
Lucas David Traverso <lucas6246@gmail.com>
2016-04-10 02:59:42 -0300

    Improved command line parsing

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,18 @@ module Main where
 
 import Lib
 import System.Environment
+import Data.Maybe (listToMaybe)
 import Text.Read (readMaybe)
 
 main :: IO ()
 main = do
-  n <- readMaybe . head <$> getArgs :: IO (Maybe Day)
+  n <- (\n -> return n >>= headMaybe >>= readMaybe) <$> getArgs :: IO (Maybe Day)
   case n of
     Nothing -> showHelp
     Just n -> putStrLn $ show $ weatherAt $ n `pmod` 360
+
+headMaybe :: [a] -> Maybe a
+headMaybe = listToMaybe
 
 showHelp :: IO ()
 showHelp = putStrLn help
