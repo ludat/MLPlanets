02d6104c55deae44d88dbe0f7ae7cb706e198473
Lucas David Traverso <lucas6246@gmail.com>
2016-04-15 21:14:40 -0300

    Added some comments for haddock generation

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,20 +1,34 @@
 module Lib
     where
 
+-- | Enum with all the Planet types
 data Planet = Ferengi | Betasoide | Vulcano deriving (Show, Eq)
 
+-- | Representations of a point in space
 data Point = Point { x :: Distance, y :: Distance} deriving (Show)
 
-type Region = Point -> Bool
-
+-- | Dummy type for angles
 type Angle = Double
+-- | Dummy type for days
 type Day = Double
+-- | Dummy type for speeds
 type Speed = Double
 
+-- | Dummy type for distances in space
 type Distance = Double
+-- | Dummy type for derivated of distance in space
 type Distance' = Double
 
+-- | A line is described as two points
+data Line = Line { from :: Point, to :: Point}
 
+-- | Side is useful to tell to which side of a line is a point
+data Side = Above | Below deriving (Show, Eq)
+
+-- | Slope is useful to tell if a distance is growing or shrinking
+data Slope = Descending | Flat | Ascending deriving (Show, Eq, Ord)
+
+-- | The weather of the planets which depends on their relative positions
 data Weather = Drought
              | HeavyRain
              | Rain
@@ -28,16 +42,25 @@ instance Show Weather where
   show Perfect    = "That day the weather will be: optimal conditions"
   show Unknown    = "I don't know how the weather will be that day"
 
+-- | Entry point for most programs which checks the weather conditions for a given day
 weatherAt :: Day -> Weather
 weatherAt n
+  -- If the sun is aligned with all the planets we have a drought
   | isSunAlignedWithPlanetsAt n = Drought
+  -- If the sun is inside the triangle at the beginning or the end of the day
+  -- then the weather is rain (possibly heavy rain)
   | isSunInsideTriangleAt n || isSunInsideTriangleAt (n+1) =
+    -- If the slope at the beginning of the day was ascending and it's descending
+    -- by the end then we have a peek of rain
     if slopeOfTriangleAt n == Ascending && slopeOfTriangleAt (n+1) == Descending  then
       HeavyRain
     else
       Rain
+  -- If planet A was at one side of the other planets and at the end of
+  -- the day it's on the other side then that day the planets were aligned
   | sideOfOtherPlanet n /= sideOfOtherPlanet (n+1) &&
     not (isSunAlignedWithPlanetsAt (n+1)) = Perfect
+  -- otherwise we don't know what the weather will look like
   | otherwise = Unknown
 
 sideOfOtherPlanet :: Day -> Maybe Side
@@ -46,20 +69,18 @@ sideOfOtherPlanet n = sideOf (positionAfterDays n Vulcano)
                               , to = positionAfterDays n Betasoide
                               }
 
-
 pmod' :: (Num a, Ord a) => a -> a -> a
 pmod' i n
   | i < 0 = until (>= 0) (+ n) i
   | i >= n = until (< n) (subtract n) i
   | otherwise = i
 
-
-
 pmod :: Angle -> Angle -> Angle
 pmod = pmod'
 
--- myTrace a b = trace (a ++ show b) b
-
+-- | Using the angles check if the sun is inside the triangle
+-- if for each planet the other two planets are on different sides of the sun
+-- then the sun must be inside the triangle that all planets form
 isSunInsideTriangle :: Angle -> Angle -> Angle -> Bool
 isSunInsideTriangle a1 a2 a3 =
   (
@@ -83,13 +104,11 @@ isSunInsideTriangle a1 a2 a3 =
     )
   )
 
+-- | Check if the angles of planets are all aligned with the sun
+-- If mod 180 all angles are the same then all planets are aligned
 isSunAlignedWithPlanets :: Angle -> Angle -> Angle -> Bool
 isSunAlignedWithPlanets a1 a2 a3 =
   (a1 `pmod` 180) == (a2 `pmod` 180) && (a2 `pmod` 180) == (a3 `pmod` 180)
-
-data Line = Line { from :: Point, to :: Point}
-
-data Side = Above | Below deriving (Show, Eq)
 
 isSunInsideTriangleAt :: Day -> Bool
 isSunInsideTriangleAt n = isSunInsideTriangle
@@ -105,6 +124,9 @@ isSunAlignedWithPlanetsAt n =
     a3 = angleAfterDays n Betasoide
   in isSunAlignedWithPlanets a1 a2 a3
 
+-- | Check to which side of a line a point is
+--
+-- If the point is on the line then return `Nothing`
 sideOf :: Point -> Line -> Maybe Side
 sideOf p l =
   let
@@ -175,8 +197,6 @@ distanceBetweenAllPlanets' n =
   distanceBetweenPlanets' n Vulcano Betasoide +
   distanceBetweenPlanets' n Ferengi Betasoide
 
-data Slope = Descending | Flat | Ascending deriving (Show, Eq, Ord)
-
 distanceBetweenPlanets2 :: Day -> Planet -> Planet -> Distance
 distanceBetweenPlanets2 day p1 p2 =
   let
@@ -190,6 +210,7 @@ toRadians n = n * pi / 180
 toDegrees :: Angle -> Angle
 toDegrees n = n * 180 / pi
 
+-- | Get the angle of a planet at a given day
 positionAfterDays :: Day -> Planet -> Point
 positionAfterDays day planet =
   let
@@ -197,14 +218,17 @@ positionAfterDays day planet =
     h = distanceFromTheSun planet
   in Point { x = cos angle * h, y = sin angle * h }
 
+-- | Get the angle of a planet at a given day
 angleAfterDays :: Day -> Planet -> Angle
 angleAfterDays day planet = (day * speed planet) `pmod` 360
 
+-- | Get the distance from the Sun of a given planet
 distanceFromTheSun :: Planet -> Distance
 distanceFromTheSun Ferengi = 500000
 distanceFromTheSun Betasoide = 2000000
 distanceFromTheSun Vulcano = 1000000
 
+-- | Get the speed of a given planet
 speed :: Planet -> Speed
 speed Ferengi = 1
 speed Betasoide = 3
